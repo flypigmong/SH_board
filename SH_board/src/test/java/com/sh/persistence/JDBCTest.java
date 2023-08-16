f8ea9a82d71c273a2478403881bf9d47429814bb
flypigmong <jseonghun85@gmail.com>
2023-08-16 19:08:11 +0900

    MySQL JDBC 테스트

---
+++ SH_board/src/test/java/com/sh/persistence/JDBCTest.java
@@ -0,0 +1,32 @@
+package com.sh.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTest {
+	
+    static { 
+        try { 
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+        } catch(Exception e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+    
+    @Test 
+    public void testConnection() { 
+        try(Connection con = DriverManager.getConnection( 
+                "jdbc:mysql://127.0.0.1:3307/book?serverTimezone=Asia/Seoul", 
+                "castello", 
+                "0000")){ 
+            System.out.println(con); 
+        } catch (Exception e) { 
+            fail(e.getMessage()); 
+        } 
+    
+    }   
+}
