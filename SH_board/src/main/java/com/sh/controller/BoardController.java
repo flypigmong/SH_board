71bdb6ba2a3ae9f96ec05bbfbf3437deea6d7cdb
flypigmong <jseonghun85@gmail.com>
2023-08-17 16:44:30 +0900

    게시판 등록기능 구현2- controller,jsp 마무리

--- SH_board/src/main/java/com/sh/controller/BoardController.java
+++ SH_board/src/main/java/com/sh/controller/BoardController.java
@@ -2,16 +2,25 @@ package com.sh.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.sh.model.BoardVO;
+import com.sh.service.BoardService;
 
 @Controller
 @RequestMapping("/board/*")
 public class BoardController {
 
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
-
+   
+	@Autowired
+    private BoardService service;
+	
     @GetMapping("/list")
     public void boardListGET() {
         
@@ -26,8 +35,19 @@ public class BoardController {
         
     }	
 
-
-
+    /* 게시판 등록 */
+    @PostMapping("/enroll")
+    public String boardEnrollPOST(BoardVO board, RedirectAttributes rttr) {
+        
+        log.info("BoardVO : " + board);
+        
+        service.enroll(board);
+        
+        rttr.addFlashAttribute("result", "enroll success");
+        
+		return "redirect:/board/list";
+        
+    }
 
 
 
